535cd4191be545ee938357dc13ff9a8cb695cb05
Hailan Pang <hpang@mit.edu>
2015-04-15 18:51:33 -0400

    more budget page changes

--- budget.js
+++ budget.js
@@ -2,6 +2,9 @@
 // has finished loading in the browser.
 $(function() {
 
+    var total_budget = 40000; //Set total budget here -- dummy data
+    document.getElementById('total_budget').innerHTML = total_budget;
+
     editable();
     calculate();
 
@@ -43,18 +46,26 @@ $(function() {
     //Add row for entered item
     function populate_list(category, spent, allotted) {
         var next_row = '<div class="row">' 
+        + '<div class="col-md-1"><span class="glyphicon glyphicon-heart pull-right" aria-hidden="true"></span></div>' 
         + '<div class="editable category col-md-4">' + category + '</div>' 
         + '<div class="editable spent col-md-2">' + spent + '</div>' 
-        + '<div class="editable allotted col-md-2">' + allotted + '</div>' + '</div>';
+        + '<div class="editable allotted col-md-2">' + allotted + '</div>'
+        + '<div class="btn trash col-md-1"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></div>'
+        + '</div>';
         return next_row;
     }
+
+    //Delete with dialog box when clicking trash icon
+    $('.trash').click(function(){
+
+    });
+
 
     //Get totals
     function calculate() {
         $(".editable.spent").each(function() {
             var class_name = ".editable.spent";
             var total_spent = calculateTotal(class_name);
-            //console.log(total_spent);
             $("#total_spent").html(total_spent);
         });
 
@@ -62,9 +73,11 @@ $(function() {
         $(".editable.allotted").each(function() {
             var class_name = ".editable.allotted";
             var total_allotted = calculateTotal(class_name);
-            //console.log(total_allotted);
             $("#total_allotted").html(total_allotted);
+            $("#remaining_budget").html(total_budget - total_allotted);
         });
+ 
+        
     }
 
     function calculateTotal(class_name) {
@@ -88,6 +101,7 @@ $(function() {
 
             //Reference the TextBox.
             var textbox = $(this).next();
+            textbox.addClass(label.attr('class'));
 
             //Set the name attribute of the TextBox.
             textbox[0].name = this.id.replace("lbl", "txt");
@@ -118,7 +132,16 @@ $(function() {
         });
     }
 
-     
-
+    //Delete confirmation
+    $('.delete_confirm').on('click', function(e) {
+        $('#confirm')
+            .modal({
+                backdrop: 'static',
+                keyboard: false
+            })
+            .one('click', '#delete', function(e) {
+                //delete function
+            });
+    });
 
 });
