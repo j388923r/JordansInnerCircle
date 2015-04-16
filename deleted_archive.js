984dc9482ff96bf5f74aca360b324ede32979b8b
Hailan Pang <hpang@mit.edu>
2015-04-16 01:04:42 -0400

    ;askdjfkasldjf develeted archive

--- deleted_archive.js
+++ deleted_archive.js
@@ -1,8 +1,1 @@
-$(document).ready(function() 
-    
-    // call the tablesorter plugin 
-    $("#myTable").tablesorter({ 
-        // sort on the first column and third column, order asc 
-        sortList: [[0,0],[2,0]] 
-    }); 
-); 
+
